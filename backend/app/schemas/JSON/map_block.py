245db82913362b0d6c6672e76e812f5d84b1753d
36alma <85577434+36alma@users.noreply.github.com>
2026-03-03 20:54:35 +0100

    feat: Add map block and map schemas, and an initial rover service.

---
+++ backend/app/schemas/JSON/map_block.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+class MapTypes(Enum):
+    AIR = "air"
+    WALL = "wall"
+    BLUE_ORE = "blue_ore"
+    YELLOW_ORE = "yellow_ore"
+    GREEN_ORE = "green_ore"
+
+class BaseMapBlock():
+    def __init__(self):
+        self.type:MapTypes
+
+class AirMapBlock(BaseMapBlock):
+    def __init__(self):
+        self.type = MapTypes.AIR
+
+class WallMapBlock(BaseMapBlock):
+    def __init__(self):
+        self.type = MapTypes.WALL
+
+class BlueOreMapBlock(BaseMapBlock):
+    def __init__(self):
+        self.type = MapTypes.BLUE_ORE
+
+class YellowOreMapBlock(BaseMapBlock):
+    def __init__(self):
+        self.type = MapTypes.YELLOW_ORE
+
+class GreenOreMapBlock(BaseMapBlock):
+    def __init__(self):
+        self.type = MapTypes.GREEN_ORE
