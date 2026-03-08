e7e6df164c4ee59379c6ca3249a5b982779dd5e4
36alma <85577434+36alma@users.noreply.github.com>
2026-03-08 23:51:03 +0100

    feat: Implement map representation, pathfinding, and initial ore clustering algorithms.

--- backend/app/schemas/JSON/cors.py
+++ backend/app/schemas/JSON/cors.py
@@ -3,3 +3,11 @@ class Cors():
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Cors):
+            return False
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
