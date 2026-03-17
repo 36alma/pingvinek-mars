49ec8f40d109fb5f682fd33ab8d7479f313f81d7
36alma <85577434+36alma@users.noreply.github.com>
2026-03-17 21:05:18 +0100

    feat: Implement rover API router with route generation and add comprehensive API documentation.

--- backend/api/v1/rover_router.py
+++ backend/api/v1/rover_router.py
@@ -146,30 +146,10 @@ class Rover_Router():
         return output
 
     def _register_api_endpoint(self):
-        @app.post("/move")
-        def move_rover(request: RoverMoveRequest):
-            return request
 
         @app.get("/start_position")
         def start_position():
             return RoverService().startpost()
-
-        @app.get("/base_route")
-        def base_route():
-            while True:
-                one_x =random.randint(0,49)
-                one_y =random.randint(0,49)
-                two_x =random.randint(0,49)
-                two_y =random.randint(0,49)
-                if isinstance(MapService().get_map_block_type(one_x,one_y),WallMapBlock):
-                    one_x =random.randint(0,49)
-                    one_y =random.randint(0,49)
-                if isinstance(MapService().get_map_block_type(two_x,two_y),WallMapBlock):
-                    two_x =random.randint(0,49)
-                    two_y =random.randint(0,49)
-                    print(two_x)
-                break
-            return OreDistanceService().get_ore_distance(ore_one_x=one_x,ore_one_y=one_y,ore_two_x=two_x,ore_two_y=two_y)
 
         @app.get("/route")
         def rover_route():
